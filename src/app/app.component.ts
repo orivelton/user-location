e0757662bd756a9af0164d6f095984355669d32f
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-03-30 07:01:57 -0300

    chore: Created service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MapsService } from './maps.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'user-location';
+  lat: string = '';
+  lng: string = '';
+  title: string = '';
+
+  location: Object;
+
+  constructor(private map: MapsService) {}
+
+  ngOnInit() {
+    this.map.getLocation().subscribe(data => {
+      console.log(data);
+      this.lat = data.longitude;
+      this.lng = data.longitude;
+      this.title = data.country_name;
+    });
+  }
+
+
+
 }
--- src/app/maps.service.ts
+++ src/app/maps.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Location {
+  latitude: string;
+  longitude: string;
+  country_name: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapsService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getLocation() {
+    return this.http.get<Location>('http://api.ipapi.com/api/161.185.160.93?access_key=');
+  }
 }
